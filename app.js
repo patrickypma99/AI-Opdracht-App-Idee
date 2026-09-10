b6c5695fbc8417df0d23862d26342c540e53237d
Patrick Ypma <patrickypma99@gmail.com>
2026-09-10 10:25:52 +0200

    Eerste versie van AI opdracht project

---
+++ app.js
@@ -0,0 +1,23 @@
+function openPage(page) {
+
+    if (page === "zoeken") {
+        alert("Hier kun je iemand zoeken.");
+    }
+
+    else if (page === "videogesprekken") {
+        alert("Hier vind je jouw videogesprekken.");
+    }
+
+    else if (page === "afspraken") {
+        alert("Hier vind je jouw afspraken.");
+    }
+
+    else if (page === "profiel") {
+        alert("Hier kun je jouw profiel bekijken.");
+    }
+
+    else if (page === "instellingen") {
+        alert("Hier kun je de instellingen aanpassen.");
+    }
+
+}
